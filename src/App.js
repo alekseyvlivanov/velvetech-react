46a56fb4cc567e8c311a629981846844b4b3bd7c
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2021-05-09 19:26:53 +1000

    init: start Velvetech React task

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="row">
+      <div className="col-md-6 mx-auto">
+        <div className="card card-body text-center mt-5">
+          <h1 className="heading pb-3">Velvetech React</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
